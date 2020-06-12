4ade0a08a5a25aa495f3db358ce059c766056d4d
Mantas Bandonis <43992327+mantasbandonis@users.noreply.github.com>
2020-06-12 12:20:31 +0200

    Description of categorical to numerical

--- Group_Project.R
+++ Group_Project.R
@@ -7,7 +7,9 @@ my_data <- read.table("dataset_Facebook.csv", sep=";", header=T)
 colSums(is.na(my_data))
 
 
-# Convert cateogrical values to integers
+# Convert cateogrical values (type of facebook content) to integers
+# Link = 1, Photo = 2, Status = 3, Video = 4
+
 my_data$Type <- as.numeric(as.factor(my_data$Type))
 
 
