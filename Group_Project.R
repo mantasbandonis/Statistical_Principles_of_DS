a84f641b521b6d72e5e1a63a5e05e1112af5dfcd
Mantas Bandonis <mantas.bandonis@ibm.com>
2020-06-17 10:56:46 +0200

    Changed project - insurance data

--- Group_Project.R
+++ Group_Project.R
@@ -1,13 +1,32 @@
-
 library("readr")
-my_data <- read.table("dataset_Facebook.csv", sep=";", header=T)
+my_data <- read.table("insurance.csv", sep=",", header=T)
 
+my_data_num <- my_data
 
-# Check how many values are missing
+# Check how many values are missing, no missing values
 colSums(is.na(my_data))
 
 
-# Convert cateogrical values to integers
-my_data$Type <- as.numeric(as.factor(my_data$Type))
+# Check types of variables in the data
+str(my_data)
+
+# TO-DO Here Visualizations and descriptive statistics without numeric values
 
 
+
+
+# Converting Smoker and Sex into numeric Values
+# Female 1, Male 0 
+
+my_data_num$sex <- ifelse(my_data$sex == "female", 1,0)
+my_data_num$sex = as.factor(my_data_num$sex )
+
+# Smoker 1, Non-smoker 0 
+
+my_data_num$smoker <- ifelse(my_data$smoker == "yes", 1,0)
+my_data_num$sex = as.factor(my_data_num$smoker)
+
+
+# Only use this if you want the regions in numeric
+
+my_data_num$region <- as.numeric(as.factor(my_data$Type))
